ea66751e11720a2e801b2a2ef07887b254c5888c
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2024-08-24 13:38:41 +0800

    feat: create chat list UI

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
+
+const Chat = () => {
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
+
+  const handleEmoji = e => {
+    setText(prev => prev + e.emoji)
+    setOpen(prev => !prev)
+  }
+
+  console.log(text)
+
+  return (
+    <div className='chat'>
+
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Last seen</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+
+      <div className="center">
+
+      </div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" placeholder="Type a message..." value={text} onChange={e => setText(e.target.value)} />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={e => setOpen(prev => !prev)} />
+          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Chat
