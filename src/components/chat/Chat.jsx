b7f9c47d735af212dbdb1a0bdb1c16f103c19b42
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2024-08-24 18:55:54 +0800

    feat: create UI for personal chat messages and recipient info

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import "./chat.css"
 import EmojiPicker from "emoji-picker-react"
 
@@ -8,10 +8,14 @@ const Chat = () => {
 
   const handleEmoji = e => {
     setText(prev => prev + e.emoji)
-    setOpen(prev => !prev)
+    setOpen(false)
   }
 
-  console.log(text)
+  const endRef = useRef(null)
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth"})
+  }, [])
 
   return (
     <div className='chat'>
@@ -32,7 +36,45 @@ const Chat = () => {
       </div>
 
       <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
 
+        <div className="message own">
+          <div className="texts">
+            <p>Hello</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Hi</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <img src="https://iili.io/fwR2zg.jpg" alt="" />
+            <p>Hello</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Hi</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
       </div>
 
       <div className="bottom">
@@ -43,8 +85,10 @@ const Chat = () => {
         </div>
         <input type="text" placeholder="Type a message..." value={text} onChange={e => setText(e.target.value)} />
         <div className="emoji">
-          <img src="./emoji.png" alt="" onClick={e => setOpen(prev => !prev)} />
-          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
         </div>
         <button className="sendButton">Send</button>
       </div>
